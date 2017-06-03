17401c87c4ce2590900312b428bf0ffbbfffd9d1
vishal84 <vishal3@gmail.com>
2017-06-03 19:33:52 -0400

    first attempt at /products list transaction

--- index.js
+++ index.js
@@ -17,9 +17,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', function(req, res) {
-  res.send('REST API endpoint');
+app.get('/products', function(req, res) {
+	var ref = db.ref("edgedemo/products");
+
+	// Attach an asynchronous callback to read the data at our posts reference
+	ref.on("value", function(snapshot) {
+	  res.send(snapshot.val());
+	  console.log(snapshot.val());
+	}, function (errorObject) {
+	  console.log("The read failed: " + errorObject.code);
+	});
 })
+
+
 
 // start app
 app.listen(port, function() {
