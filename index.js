51e12779603867398ae45f464932881c6b7cb0c3
vishal84 <vishal3@gmail.com>
2017-06-03 19:25:31 -0400

    adding express

--- index.js
+++ index.js
@@ -6,15 +6,23 @@ admin.initializeApp({
   databaseURL: "https://edgedemo-925ea.firebaseio.com/"
 });
 
-// Get a database reference to our blog
+// Get a reference to Firebase DB for products, orders, etc
 const db = admin.database();
 const ref = db.ref("edgedemo");
 
 const productRef = ref.child("products");
 
+// Create an app using express for REST APIs
+const express = require('express');
+const app = express();
+const port = 3000;
 
+app.get('/', function(req, res) {
+  res.send('REST API endpoint');
+})
 
-
-
-
+// start app
+app.listen(port, function() {
+  console.log('listening on 3000')
+})
 
