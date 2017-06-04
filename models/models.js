09fc4acf277872da20a3115dcd4ed00b27d36f3f
vishal84 <vishal3@gmail.com>
2017-06-04 13:40:50 -0400

    added models

---
+++ models/models.js
@@ -0,0 +1,37 @@
+
+var Order = {
+  cart: {
+    products: []
+  },
+  customer: {
+    email: "",
+    first: "",
+    id: "",
+    last: "",
+    shipping: {
+      city: "",
+      state: "",
+      street: "",
+      zip: ""
+    }
+  },
+  id: "",
+  payment: {
+    cardNumber: "",
+    cvv: "",
+    month: "",
+    year: ""
+  },
+  state: "",
+  tracking: ""
+}
+
+var Product = {
+    description: "",
+    id: "",
+    image: "",
+    price: "",
+    sku: "",
+    title: "",
+    type: ""
+  }
