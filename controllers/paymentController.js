d335321208489b3ef20ed229d2d6c78ebbb4de77
vishal84 <vishal3@gmail.com>
2017-06-04 15:34:06 -0400

    added payments

---
+++ controllers/paymentController.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// Get Firebase object and service account key
+const admin = require("firebase-admin");
+const serviceAccount = require("../edgedemo-925ea.json");
+
+var models = require('../models/models.js');
+
+// Get a reference to Firebase DB for products, orders, etc
+const db = admin.database();
+const ref = db.ref("edgedemo/payments");
+
+
+var key = ref.push();
+ref.set({
+	first: "John",
+	last: "Doe",
+	cardNumber: "1111222233334444",
+	expMonth: "10",
+	expYear: "2020",
+	cvv:
+});
+
+// GET transaction /payments
+exports.payWithPalPay = function(req, res) {
+
+	ref.on("value", function(snapshot) {
+		res.json(snapshot.val());
+		console.log(snapshot.val());
+	}, function (errorObject) {
+		res.send(errorObject);
+		console.log("The read failed: " + errorObject.code);
+	});
+};
+
+
