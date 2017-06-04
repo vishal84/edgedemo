641b2078ce1e056c67e2d74fbc9d9643df0e2a06
vishal84 <vishal3@gmail.com>
2017-06-03 22:55:24 -0400

    controller logic

---
+++ controllers/productController.js
@@ -0,0 +1,43 @@
+'use strict';
+
+// Get Firebase object and service account key
+const admin = require("firebase-admin");
+const serviceAccount = require("./edgedemo-925ea.json");
+
+// Initialize
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://edgedemo-925ea.firebaseio.com/"
+});
+
+// Get a reference to Firebase DB for products, orders, etc
+const db = admin.database();
+const ref = db.ref("edgedemo/produdcts");
+
+// /products GET transaction
+exports.getAllProducts = function(req, res) {
+
+	ref.on("value", function(snapshot) {
+		res.json(snapshot.val());
+		console.log(snapshot.val());
+	}, function (errorObject) {
+		res.send(errorObject);
+		console.log("The read failed: " + errorObject.code);
+	});
+};
+
+// /products/:productId GET transaction
+exports.getProduct = function(req, res) {
+
+	var productId = req.params.taskId;
+	ref.orderByChild("id").on("value", function(snapshot) {
+		if (snapshot.key = productId) {
+			res.json(snapshot.val());
+			console.log(snapshot.val());
+		}
+	}, function(errorObject) {
+		res.send(errorObject);
+		console.log("The read failed: " + errorObject.code);
+	});
+};
+
