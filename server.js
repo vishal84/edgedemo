6fc2c71be9f2eb5b058d9dc1edb850cc9df4f1f6
vishal84 <vishal3@gmail.com>
2017-06-03 22:58:57 -0400

    add routes to app

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require('./edgedemo/routes/routes');
+routes(app);
+
 // start app
 app.listen(port, function() {
   console.log('Edge Demo REST API listening on port: ' + port);
