7da6457bb4075637bfdc1a41d49d4bdb120a03f4
vishal84 <vishal3@gmail.com>
2017-06-05 00:36:16 -0400

    add headers

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ routes(app);
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
