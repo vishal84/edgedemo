dc38785aa6df76a43d18a975c292e59f8ec68ea6
vishal84 <vishal3@gmail.com>
2017-06-06 01:12:52 -0400

    use cors...

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Create an app using express for REST APIs
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // init app settings
 const app = express();
@@ -8,19 +9,12 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors);
 
 var routes = require('./routes/routes.js');
 routes(app);
 
 // start app
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Origin");
-  next();
-});
-
 app.listen(port, function() {
   console.log('Edge Demo REST API listening on port: ' + port);
 })
